9e9d50c1f1f6b28e1ea0ed1358cc8414ceb870e9
Shreycode27 <yshrey127@gmail.com>
2026-03-10 22:01:18 +0530

    Day 2: Separate sections for Arithmetic and Division

---
+++ Day-02/arithemetic.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    print("---section A--- : Arithemetic (+,-,*)---")
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    # arithemetic answers
+
+    print("Sum : ",a+b)
+    print("diffrence :", a-b)
+    print("product :", a*b)
+
+    print("---section B --- : Division(/,//,%)")
+    c = int(input("enter the first divison number : "))
+    d = int(input("enter the second divison number : "))
+    # division answers 
+
+    print("integer divison :", a/b)
+    print("float divison:", a//b)
+    print("modulus  :", a%b)
