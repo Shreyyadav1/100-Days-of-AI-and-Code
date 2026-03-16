56a909d54c5cc1235e3faccaaed27e8ba3620b29
Shreycode27 <yshrey127@gmail.com>
2026-03-16 20:27:07 +0530

    Day-08 find runner up score

--- Day-08/find runner up score.py
+++ Day-08/find runner up score.py
@@ -1,10 +1,11 @@
 if __name__ == '__main__':
     n = int(input())
+     # 1. Read input and convert it into a list of integers
     arr = list(map(int, input().split()))
 
     max_score = max(arr)
 
     while max_score in arr:
-      arr.remove(max_score)
+      arr.remove(max_score) #remove duplicate values
 
 print(max(arr))
