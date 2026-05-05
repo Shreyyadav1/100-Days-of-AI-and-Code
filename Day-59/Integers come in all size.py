2f588041e92ae87ebb38f55bf109aa5264bc238a
Shreycode27 <yshrey127@gmail.com>
2026-05-05 09:53:22 +0530

    Day 59: Demonstrate Python's native arbitrary-precision integer arithmetic

---
+++ Day-59/Integers come in all size.py
@@ -0,0 +1,17 @@
+# Step 1: Take inputs
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+# Step 2: Calculate a^b
+first = pow(a, b)
+
+# Step 3: Calculate c^d
+second = pow(c, d)
+
+# Step 4: Add both results
+result = first + second
+
+# Step 5: Print final result
+print(result)
